04beccd91470656ec6944e54d10aa706f2ffd43b
Park KiJun <ekdlwns3685@naver.com>
2019-11-04 18:36:47 +0900

    Added presenter structures

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,22 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+  movieResults,
+  TVResults,
+  loading,
+  searchTerm,
+  error
+}) => null;
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  TVResults: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  searchTerm: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
